688488b3dbab78501049867d9fb296882a75b2d0
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-04 23:38:49 +0600

    creare News Component using Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.css';
+import News from './Components/News/News';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Button color="primary">Hello World</Button>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+            <News></News>
+        </div>
+    );
 }
 
 export default App;
